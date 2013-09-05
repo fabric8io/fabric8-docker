1932b6bae08926f84dfaf5733e1feae6017fb259
James Strachan <james.strachan@gmail.com>
2013-09-05 12:33:36 +0100

    updated instructions and added a stand alone contianer

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 echo "building the Docker containers for Fuse and Fuse Fabric"
 sudo docker build -t java java
 sudo docker build -t fuse fuse
+sudo docker build -t fuse-standalone fuse-standalone
+
