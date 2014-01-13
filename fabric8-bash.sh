2998f21432ce164d001fb05dac5aaa03a00455f5
James Strachan <james.strachan@gmail.com>
2014-01-13 14:00:22 +0000

    updated the docker contianer for fabric8

---
+++ fabric8-bash.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Starting the fabric8 container"
+docker run -p 8181 -i -t fabric8:fabric8/bin/bash
---
+++ fabric8.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Starting the fabric8 container"
+docker run -p 8181 -d -t fabric8:fabric8
