3e367d0c0d333327a320d3beb3e6db840c891f90
James Strachan <james.strachan@gmail.com>
2014-03-19 11:13:42 +0000

    try using centos to run sshd

--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@
 echo starting Fabric8 container: $FABRIC8_KARAF_NAME connecting to ZooKeeper: $FABRIC8_ZOOKEEPER_URL using environment: $FABRIC8_FABRIC_ENVIRONMENT
 
 # TODO allow this to be disabled via an env var
-/usr/sbin/sshd -D &
+service sshd start
+#/usr/sbin/sshd -D &
 
 # TODO if enabled should we tail the karaf log to work nicer with docker logs?
 #tail -f /home/fuse/fabric8/data/log/karaf.log
