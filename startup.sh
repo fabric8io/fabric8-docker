e0b7f4e4882098ed142fb5276b40bce45d53d550
James Strachan <james.strachan@gmail.com>
2014-09-03 16:22:28 +0100

    polished startup output

--- startup.sh
+++ startup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
+ADMIN_PASSWORD=${ADMIN_PASSWORD:-admin}
+
 echo Welcome to fabric8: http://fabric8.io/
 echo
 echo Starting Fabric8 container ID: $FABRIC8_RUNTIME_ID
-echo Connecting to ZooKeeper: $FABRIC8_ZOOKEEPER_URL using environment: $FABRIC8_FABRIC_ENVIRONMENT
-echo Using bindaddress: $FABRIC8_BINDADDRESS
-
-ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-admin}
+echo Using admin user:              ${ADMIN_USERNAME}
+echo Connecting to ZooKeeper:       $FABRIC8_ZOOKEEPER_URL 
+echo Environment:                   $FABRIC8_FABRIC_ENVIRONMENT
+echo Using bindaddress:             $FABRIC8_BINDADDRESS
 
 echo "admin=${ADMIN_USERNAME},${ADMIN_PASSWORD}" >> users.properties
 
