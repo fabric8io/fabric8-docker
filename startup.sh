5809bce3465f4c3c673d923d480172642d8b96c8
Wouter Scheele <w.scheele@secondfloor.com>
2014-09-16 12:55:10 +0200

    configure karaf.name/runtime.id from docker env, and fix users.properties addition

--- startup.sh
+++ startup.sh
@@ -12,7 +12,11 @@ echo Connecting to ZooKeeper:       $FABRIC8_ZOOKEEPER_URL
 echo Environment:                   $FABRIC8_FABRIC_ENVIRONMENT
 echo Using bindaddress:             $FABRIC8_BINDADDRESS
 
-echo "admin=${ADMIN_USERNAME},${ADMIN_PASSWORD}" >> users.properties
+grep -q "${ADMIN_USERNAME}=${ADMIN_PASSWORD},admin,manager,viewer" /opt/fabric8/etc/users.properties || echo "${ADMIN_USERNAME}=${ADMIN_PASSWORD},admin,manager,viewer" >> /opt/fabric8/etc/users.properties
+
+# lets base the karaf.name on the configured environment variable.
+sed -i "s/^karaf.name=root$/karaf.name=${FABRIC8_RUNTIME_ID}/" /opt/fabric8/etc/system.properties
+
 
 # Use exec to replace shell with process to ensure signals get handled correctly
 exec /opt/fabric8/bin/fabric8 server
